49d9ee279681cb6a89c2edfd4de756ec846fdeaf
ik123-coder <ahmadleftigr@gmail.com>
2026-02-26 01:50:19 +0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,12 @@
-n
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#0f0f0f',
+        green: '#22c55e'
+      }
+    }
+  }
+}
