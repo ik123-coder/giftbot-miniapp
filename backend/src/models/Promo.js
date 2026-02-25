dcc21c74beb83bb3d410a5019917be151bef9f82
ik123-coder <ahmadleftigr@gmail.com>
2026-02-26 01:47:20 +0300

    Update Promo.js

--- backend/src/models/Promo.js
+++ backend/src/models/Promo.js
@@ -1,1 +1,10 @@
-n
+const mongoose = require('mongoose');
+
+const PromoSchema = new mongoose.Schema({
+  code: { type: String, required: true, unique: true },
+  reward: { type: Number, required: true },
+  maxUses: { type: Number, default: 1 },
+  usedBy: [{ type: Number }]
+});
+
+module.exports = mongoose.model('Promo', PromoSchema);
