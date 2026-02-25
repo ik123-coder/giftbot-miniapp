96fc311acd7f9cacdeb72a49b3daa045e7d754c3
ik123-coder <ahmadleftigr@gmail.com>
2026-02-26 01:46:15 +0300

    Update validateInitData.js

--- backend/src/utils/validateInitData.js
+++ backend/src/utils/validateInitData.js
@@ -1,1 +1,26 @@
-n
+const crypto = require('crypto');
+
+const validateInitData = (initData, botToken) => {
+  const secret = crypto
+    .createHmac('sha256', 'WebAppData')
+    .update(botToken)
+    .digest();
+
+  const data = new URLSearchParams(initData);
+  const hash = data.get('hash');
+  data.delete('hash');
+
+  const dataCheckString = Array.from(data.entries())
+    .sort(([a], [b]) => a.localeCompare(b))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('\n');
+
+  const hmac = crypto
+    .createHmac('sha256', secret)
+    .update(dataCheckString)
+    .digest('hex');
+
+  return hmac === hash;
+};
+
+module.exports = validateInitData;
