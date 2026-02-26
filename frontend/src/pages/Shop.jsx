8e26d5c2744ad34c3d905a1c0b75b62fbe926456
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 02:52:28 +0300

    Delete frontend directory

--- frontend/src/pages/Shop.jsx
+++
@@ -1,9 +0,0 @@
-const Shop = () => (
-  <div className="p-5 pb-24 flex flex-col items-center justify-center h-[70vh] text-center">
-    <div className="text-6xl mb-6">🎁</div>
-    <div className="text-3xl font-bold mb-3">Магазин пока что недоступен!</div>
-    <div className="text-gray-400 max-w-xs">Скоро тут будут кейсы и крутые призы</div>
-  </div>
-);
-
-export default Shop;
