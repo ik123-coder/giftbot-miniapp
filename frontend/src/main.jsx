9bb31f47fba4520faafe1e80cb1461fb761df741
ik123-coder <ahmadleftigr@gmail.com>
2026-02-26 05:23:36 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { UserProvider } from './context/UserContext'  // ← добавь импорт
+import { UserProvider } from './context/UserContext.jsx'  // ← добавь импорт
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
