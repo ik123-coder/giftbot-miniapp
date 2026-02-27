68e00b8e7d8e7ef12134e95e293826d7c772f124
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 06:05:56 +0300

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
 
+const userRoutes = require('./userRoutes');
+const taskRoutes = require('./taskRoutes');
+const promoRoutes = require('./promoRoutes');
+const referralRoutes = require('./referralRoutes');
+const giveawayRoutes = require('./giveawayRoutes');
+
+router.use('/user', userRoutes);
+router.use('/tasks', taskRoutes);
+router.use('/promos', promoRoutes);
+router.use('/referrals', referralRoutes);
+router.use('/giveaways', giveawayRoutes);
+
+module.exports = router;
