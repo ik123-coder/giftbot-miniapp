4a2144a7cb3263952c7cada3e1fb64f941a099f2
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 06:07:03 +0300

    Update referralService.js

--- server/src/services/referralService.js
+++ server/src/services/referralService.js
@@ -1,1 +1,12 @@
+class ReferralService {
+  static async processReferral(referrerId, newUserId) {
+    // TODO: позже
+    throw new Error('Рефералка пока недоступна');
+  }
 
+  static async getReferralStats(userId) {
+    throw new Error('Статистика рефералов временно недоступна');
+  }
+}
+
+module.exports = ReferralService;
