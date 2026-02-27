fcfe20970f8a7dfd2348094c314ac1ce7e0a4189
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 06:02:39 +0300

    Update index.js

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,1 +1,9 @@
+require('dotenv').config();
 
+module.exports = {
+  port: process.env.PORT || 8080,
+  mongoUri: process.env.MONGO_URI,
+  botToken: process.env.BOT_TOKEN,
+  nodeEnv: process.env.NODE_ENV || 'development',
+  // можно добавить jwtSecret, redisUrl и т.д. позже
+};
