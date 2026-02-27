279e305a1edbda88facc6f1ba8bc9a106bda2fba
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 13:36:24 +0300

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,18 +24,18 @@ export default function Header({ firstName, balance, photoUrl }) {
 
       {/* Правая часть: баланс */}
       <div
-        className="
-          flex items-center gap-1
-          px-4 py-2
-          rounded-full
-          bg-[#2b2b2b]
-          text-white
-          shadow-[0_0_8px_rgba(34,197,94,0.7),0_0_20px_rgba(34,197,94,0.4)]
-        "
-      >
-        <span className="text-green-400 text-lg">$</span>
-        <span className="font-bold">{balance}</span>
-      </div>
+  className="
+    flex items-center gap-1
+    px-4 py-2
+    rounded-full
+    bg-[#2b2b2b]  /* полностью непрозрачный */
+    text-white
+    shadow-[0_0_10px_rgba(34,197,94,0.7),0_0_25px_rgba(34,197,94,0.4)]
+  "
+>
+  <span className="text-green-400 text-lg">$</span>
+  <span className="font-bold">{balance}</span>
+</div>
     </div>
   );
 }
