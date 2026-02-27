ddc27ff716a1d1df67134b99b3df5100c03f0c55
ik123-coder <ahmadleftigr@gmail.com>
2026-02-28 02:23:21 +0300

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,16 @@
+// config/db.js
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
+const connectDB = async (mongoUri) => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    if (!mongoUri) throw new Error('Mongo URI не задан');
+
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // таймаут на поиск сервера
+    });
+
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
