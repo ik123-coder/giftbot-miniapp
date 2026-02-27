f62d0dfb32c1020102d59f54dba69c9fd0c9bb4c
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 07:09:31 +0300

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,10 +7,14 @@ const promoRoutes = require('./promoRoutes');
 const referralRoutes = require('./referralRoutes');
 const giveawayRoutes = require('./giveawayRoutes');
 
-router.use('/user', userRoutes);
-router.use('/tasks', taskRoutes);
-router.use('/promos', promoRoutes);
-router.use('/referrals', referralRoutes);
-router.use('/giveaways', giveawayRoutes);
+const express = require('express');
+const router = express.Router();
+
+// Временно отключаем все роуты, кроме базового
+// router.use('/user', userRoutes);
+// router.use('/tasks', taskRoutes);
+// router.use('/promos', promoRoutes);
+// router.use('/referrals', referralRoutes);
+// router.use('/giveaways', giveawayRoutes);
 
 module.exports = router;
