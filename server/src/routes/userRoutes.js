3bf99667a199d5a63eca0fed1982c97b098d0f47
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 05:32:02 +0300

    Update userRoutes.js

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const { getProfile, updateBalance } = require('../controllers/userController');
 
+router.get('/profile', auth, getProfile);
+router.post('/balance', auth, updateBalance);
+
+module.exports = router;
