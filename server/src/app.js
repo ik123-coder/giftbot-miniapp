db284f14a13f8d451a7c361723b62e4a01e9f74b
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 07:18:05 +0300

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,15 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/errorHandler');
-// const limiter = require('./middleware/rateLimiter');
-// app.use(limiter);
+
 const apiRoutes = require('./routes/index');
 
 const app = express();
 
-app.use(cors({ origin: '*' })); // в продакшене ограничить
+app.use(cors({ origin: '*' }));
 app.use(express.json());
-app.use(limiter);
 
 // Подключение БД
 connectDB();
@@ -19,12 +17,13 @@ connectDB();
 // Роуты
 app.use('/api', apiRoutes);
 
-// Обработка ошибок
-app.use(errorHandler);
-
+// Тестовый маршрут
 app.get('/', (req, res) => {
   res.json({ message: 'GiftBot API v1.0 - running' });
 });
+
+// Обработка ошибок
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
