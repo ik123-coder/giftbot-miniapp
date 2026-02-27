d71aab105d51479a5cd9b6c3cd55b2bc566111a9
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 06:06:27 +0300

    Update taskRoutes.js

--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getTasks, completeTask } = require('../controllers/taskController');
 
+router.get('/', getTasks);
+router.post('/complete', completeTask);
+
+module.exports = router;
