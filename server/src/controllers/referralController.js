ff12df4564fe456c8c88334012b1cbb64c09fac7
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 06:03:22 +0300

    Update referralController.js

--- server/src/controllers/referralController.js
+++ server/src/controllers/referralController.js
@@ -1,1 +1,13 @@
+exports.getReferrals = (req, res) => {
+  res.status(503).json({
+    success: false,
+    message: 'Реферальная система временно недоступна.',
+  });
+};
 
+exports.getReferralLink = (req, res) => {
+  res.status(503).json({
+    success: false,
+    message: 'Генерация реферальной ссылки пока недоступна.',
+  });
+};
