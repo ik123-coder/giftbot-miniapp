43cf71654660bf120ca973e9b6ddef4cfef07178
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 06:03:34 +0300

    Update taskController.js

--- server/src/controllers/taskController.js
+++ server/src/controllers/taskController.js
@@ -1,1 +1,13 @@
+exports.getTasks = (req, res) => {
+  res.status(503).json({
+    success: false,
+    message: 'Задания временно недоступны. Раздел в разработке.',
+  });
+};
 
+exports.completeTask = (req, res) => {
+  res.status(503).json({
+    success: false,
+    message: 'Выполнение заданий пока недоступно.',
+  });
+};
