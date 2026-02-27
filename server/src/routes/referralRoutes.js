5971cd827761ea64354007637580cb61dd9cba48
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 06:06:14 +0300

    Update referralRoutes.js

--- server/src/routes/referralRoutes.js
+++ server/src/routes/referralRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getReferrals, getReferralLink } = require('../controllers/referralController');
 
+router.get('/', getReferrals);
+router.get('/link', getReferralLink);
+
+module.exports = router;
