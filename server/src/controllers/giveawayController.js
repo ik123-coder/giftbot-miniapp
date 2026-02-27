2b76da99b8c83961e6cb328ac84a041b967d1d06
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 06:02:56 +0300

    Update giveawayController.js

--- server/src/controllers/giveawayController.js
+++ server/src/controllers/giveawayController.js
@@ -1,1 +1,13 @@
+exports.getGiveaways = (req, res) => {
+  res.status(503).json({
+    success: false,
+    message: 'Розыгрыши временно недоступны. Раздел в разработке.',
+  });
+};
 
+exports.participate = (req, res) => {
+  res.status(503).json({
+    success: false,
+    message: 'Участие в розыгрышах пока недоступно.',
+  });
+};
