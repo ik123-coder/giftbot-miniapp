7a7c2918a4d8d6b8df63f77f2a113e60e4da4d6f
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 07:15:12 +0300

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+// Импортируем роуты (все закомментированы, чтобы не ломалось)
 const userRoutes = require('./userRoutes');
-const taskRoutes = require('./taskRoutes');
-const promoRoutes = require('./promoRoutes');
-const referralRoutes = require('./referralRoutes');
-const giveawayRoutes = require('./giveawayRoutes');
+// const taskRoutes = require('./taskRoutes');
+// const promoRoutes = require('./promoRoutes');
+// const referralRoutes = require('./referralRoutes');
+// const giveawayRoutes = require('./giveawayRoutes');
 
-const express = require('express');
-const router = express.Router();
+// Подключаем только userRoutes (остальные закомментированы)
+router.use('/user', userRoutes);
 
-// Временно отключаем все роуты, кроме базового
-// router.use('/user', userRoutes);
+// Остальные роуты пока отключены, чтобы избежать ошибок
 // router.use('/tasks', taskRoutes);
 // router.use('/promos', promoRoutes);
 // router.use('/referrals', referralRoutes);
