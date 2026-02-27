c14d59243327d1f27e829e2ca1d90ccd887c3397
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 06:05:43 +0300

    Update giveawayRoutes.js

--- server/src/routes/giveawayRoutes.js
+++ server/src/routes/giveawayRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getGiveaways, participate } = require('../controllers/giveawayController');
 
+router.get('/', getGiveaways);
+router.post('/participate', participate);
+
+module.exports = router;
