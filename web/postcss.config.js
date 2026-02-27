c52a2a24b8fe2255b4171bf70265dc1b160de5e4
ik123-coder <ahmadleftigr@gmail.com>
2026-02-27 04:22:13 +0300

    Update postcss.config.js

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
